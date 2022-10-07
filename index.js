d18ed3675e4f27aed1af2646a71ebb3cd992949b
EeSienGecko <eesien.thien@geckoservices.com.my>
2022-10-07 16:48:41 +0800

    asdfasdf

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('express')();
-const PORT = 8080;
+const PORT = 3000;
 const fcl = require('@onflow/fcl');
 const t = require('@onflow/types');
 
@@ -93,7 +93,7 @@ app.listen(
     ()=> console.log('Running...')
 )
 
-app.get('/',()=>{
+app.get('/',(req, res)=>{
     res.send("<h1>Hello World!</h1>")
 });
 
